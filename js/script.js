7ece2f9c469d74cea843dc2c41da6645b1989107
tenkoff <tenkoff757513@gmail.com>
2022-04-11 03:43:10 +0700

    complite card product

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-var swiper=new Swiper('.slider', {
-    navigator: {
-        nextEL: '.slider__btn-next',
-        prevEL: '.swiper-button-prev'
+var swiper = new Swiper(".slider", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    navigation: {
+      nextEl: ".slider__btn-next",
+      prevEl: ".slider__btn-prev",
     },
-});
+  });
+let menuBtn = document.querySelector('.menu-btn');
+let menuCloseBtn = document.querySelector('.menu-close-btn');
+let menu = document.querySelector('.menu-mobile');
+menuBtn.addEventListener('click', function(){
+    menu.classList.add('menu-active');
+})
+menuCloseBtn.addEventListener('click', function(){
+    menu.classList.remove('menu-active');
+})
